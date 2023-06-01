2f16c1791bf38a1be4913ad9778aff961c4237ae
DuarteCosta <mestredua@gmail.com>
2023-06-01 16:39:00 +0100

    debug and clean

--- poke-env/src/poke_env/player/reactive_player.py
+++ poke-env/src/poke_env/player/reactive_player.py
@@ -18,7 +18,7 @@ movesetUsage["snorlax"]     = ["bodyslam", "selfdestruct", "earthquake", "hyperb
 movesetUsage["tauros"]      = ["bodyslam", "hyperbeam", "blizzard", "earthquake", "fireblast"]
 movesetUsage["chansey"]     = ["softboiled", "thunderwave", "icebeam", "thunderbolt", "seismictoss", "reflect", 
                                "counter", "sing"]
-movesetUsage["Exeggutor"]   = ["psychic", "sleeppowder", "explosion", "stunspore", "doubleedge", "hyperbeam", "megadrain", 
+movesetUsage["exeggutor"]   = ["psychic", "sleeppowder", "explosion", "stunspore", "doubleedge", "hyperbeam", "megadrain", 
                                "leechseed"]
 movesetUsage["alakazam"]    = ["thunderwave", "psychic", "seismictoss", "recover", "reflect"]
 movesetUsage["starmie"]     = ["thunderwave", "blizzard", "recover", "thunderbolt", "psychic"]
@@ -52,34 +52,56 @@ class ReactivePlayer(Player):
         opp = battle.opponent_active_pokemon
         
         available_moves = battle.available_moves
-        available_switches = battle.available_switches
 
         typeMatchup = self.getWeaknesses(battle)
-        weakArr = typeMatchup[0]
-        resiArr = typeMatchup[1]
-        immuArr = typeMatchup[2]
 
         print("***************************************************************************************************")
         print("[Turn ", battle._turn, "]")
-        print("Active Pokemon:")
-        print(me.species, me._status, int((me._current_hp/me.max_hp)*100), me.base_stats["spe"], str([move._id for move in available_moves]))
+        print("OPPONENT Pokemon:", opp.species, opp._status, opp._current_hp, opp.base_stats)
+        print("ME Active Pokemon:", me.species, me._status, int((me._current_hp/me.max_hp)*100), me.base_stats["spe"], str([move._id for move in available_moves]))
 
-        '''begin{score available moves}'''
+        best_move = self.evaluate_available_moves(available_moves, battle, me, opp)
+
+        print("Chosen_move -> ", best_move._id)
+
+        self.print_team_moves(battle)
+        self.print_Weak_Resis_Immun(typeMatchup)
+
+        return BattleOrder(best_move)
+    
+    def getSideEffectsMove(self, move):
+        sideEffectVal = 0
+        if move._id in sideEffects["FRZ"]:
+            sideEffectVal = 100
+        elif move._id in sideEffects["SLP"]:
+            sideEffectVal = 90
+        elif move._id in sideEffects["PAR"]:
+            sideEffectVal = 50
+        elif move._id in sideEffects["BRN"] or move._id in sideEffects["PSN"]:
+            sideEffectVal = 20
+        return sideEffectVal
+    
+    def get_outspeed(self, me, opp):
         if me.base_stats["spe"] > opp.base_stats["spe"]:
             outspeed = 1
         else:
             outspeed = 0
-            
+        return outspeed
+    
+    def get_reliable_recovery(self, available_moves):
         for move in available_moves:
             if move._id == "softboiled" or move._id == "recover":
                 reliableRecovery = 1
             else:
                 reliableRecovery = 0
+        return reliableRecovery
+    
+    def evaluate_available_moves(self, available_moves, battle, me, opp):
+        outspeed = self.get_outspeed(me, opp)
         opp_max_hp = floor((opp.base_stats["hp"]+15)*2+252/4)+110
         oppMaxDmg = self.get_opponent_strongest_move_damage(me, opp, battle)
-        type_chart = opp._data.type_chart
         max_score = 0
-        #print("available moves info: ")
+        best_move = available_moves[0]
         for move in available_moves:
             move_info = battle.available_moves[0]._moves_dict[move._id]
             acc = move_info["accuracy"]
@@ -95,7 +117,7 @@ class ReactivePlayer(Player):
             elif move._id == "seismictoss" or move._id == "nightshade":
                 move_dmg = 100/opp_max_hp*100
                 score_baseline = ((me._current_hp/me.max_hp)*100-oppMaxDmg)/100*(move_dmg)*(acc/100)/(oppMaxDmg+1)
-                ##add recovery impact
+                #add recovery impact
                 if move_dmg > opp._current_hp:
                     outspeed_benefit = outspeed*MAX_WEIGHT
                 else:
@@ -103,87 +125,14 @@ class ReactivePlayer(Player):
                 
                 score = score_baseline+outspeed_benefit
             else:
-                category = move_info["category"]
-                if category == "Status":
-                    move_dmg = 0
-                else:
-                    if category == "Physical":
-                        attack_stat = floor((me.base_stats["atk"]+15)*2+252/4)+5
-                        defense_stat = floor((opp.base_stats["def"]+15)*2+252/4)+5
-                        
-                    else:
-                        attack_stat = floor((me.base_stats["spa"]+15)*2+252/4)+5
-                        defense_stat = floor((opp.base_stats["spd"]+15)*2+252/4)+5
-                    move_type = move_info["type"].upper()
-                    if move_type == me._type_1:
-                        stab = 1.5
-                    elif me._type_2 is not None and move_type == me._type_2:
-                        stab = 1.5
-                    else:
-                        stab = 1
-                    type_effectiveness = type_chart[opp._type_1.name.upper()][move_type]
-                    if me._type_2 is not None:
-                        type_effectiveness *= type_chart[opp._type_2.name.upper()][move_type]
-                    move_dmg = self.calculate_damage(100, attack_stat, defense_stat, move_info["basePower"], stab, type_effectiveness)
-                    move_dmg = move_dmg/opp_max_hp*100
-                    #print(move._id, stab, type_effectiveness, move_dmg)
-
-                sideEffectsVal = 0
-                if opp._status is None:
-                    if move._id in sideEffects["FRZ"]:
-                        sideEffectVal = 100
-                    elif move._id in sideEffects["SLP"]:
-                        sideEffectVal = 90
-                    elif move._id in sideEffects["PAR"]:
-                        sideEffectVal = 50
-                    elif move._id in sideEffects["BRN"] or move._id in sideEffects["PSN"]:
-                        sideEffectVal = 20
-
-                score_baseline = ((me._current_hp/me.max_hp)*100-oppMaxDmg)/100*(move_dmg+sideEffectVal)*(acc/100)/(oppMaxDmg+1)
-                ##add recovery impact
-                if move_dmg > opp._current_hp:
-                    outspeed_benefit = outspeed*MAX_WEIGHT
-                else:
-                    outspeed_benefit = 0
-                score = score_baseline+outspeed_benefit
+                score = self.calculate_normal_move_score(move, move_info, me, opp, opp_max_hp, oppMaxDmg, outspeed)
 
             print("[", move._id, "] (", score, ")")
             if score > max_score:
                 max_score = score
                 best_move = move
-        
-        '''end{score available moves}'''
+        return best_move
 
-        print("Benched Pokemon:")
-        for available_switch in available_switches:
-            print(available_switch.species, available_switch._status, str([move for move in available_switch._moves]))
-        print("Opponent Pokemon:")
-        print(opp.species, opp._status, opp._current_hp, opp.base_stats["spe"])
-        print("Weaknesses: ", weakArr)
-        print("Resistances: ", resiArr)
-        print("Immunities: ", immuArr)
-        print("***************************************************************************************************")
-
-        #"""
-        moves_team = self.getAllMovesTeam(battle)
-        best_moves = self.find_strongest_super_effective_move(weakArr, moves_team[0][1])
-        if best_moves and self.check_if_pokemon_has_effects(battle.active_pokemon) is False:
-            print("Move -> ", best_moves[0][0])
-            return BattleOrder(best_moves[0][0]) 
-        else:
-            switch = self.find_best_switch(battle, weakArr)
-            if switch:
-                print("Switch -> ", switch[0])
-                return BattleOrder(switch[0])
-            else:
-                
-                #If the pokemon is less than half hp, switch to the highest hp pokemon that isn't weak against that oponnent
-                #else make the move with the highest move_base_power*resistance ratio
-                
-                print("Random")
-                return self.choose_random_singles_move(battle)
-        #"""
-    
     def calculate_damage(self, level, attack_stat, defense_stat, base_power, stab, type_effectiveness):
         damage = (((2*level)/5+2)*base_power*attack_stat/defense_stat)/50+2
         damage *= stab
@@ -223,6 +172,47 @@ class ReactivePlayer(Player):
                 biggest_damage = damage
         
         return biggest_damage
+
+    def calculate_normal_move_score(self, move, move_info, me, opp, opp_max_hp, oppMaxDmg, outspeed):
+        outspeed = self.get_outspeed(me, opp)
+        type_chart = opp._data.type_chart
+        acc = move_info["accuracy"]
+        category = move_info["category"]
+        if category == "Status":
+            move_dmg = 0
+        else:
+            if category == "Physical":
+                attack_stat = floor((me.base_stats["atk"]+15)*2+252/4)+5
+                defense_stat = floor((opp.base_stats["def"]+15)*2+252/4)+5
+            else:
+                attack_stat = floor((me.base_stats["spa"]+15)*2+252/4)+5
+                defense_stat = floor((opp.base_stats["spd"]+15)*2+252/4)+5
+            move_type = move_info["type"].upper()
+            if move_type == me._type_1:
+                stab = 1.5
+            elif me._type_2 is not None and move_type == me._type_2:
+                stab = 1.5
+            else:
+                stab = 1
+            type_effectiveness = type_chart[opp._type_1.name][move_type]
+            if opp._type_2 is not None:
+                type_effectiveness *= type_chart[opp._type_2.name][move_type]
+            move_dmg = self.calculate_damage(100, attack_stat, defense_stat, move_info["basePower"], stab, type_effectiveness)
+            move_dmg = move_dmg/opp_max_hp*100
+            #print(move._id, stab, type_effectiveness, move_dmg)
+
+        sideEffectsVal = 0
+        if opp._status is None:
+            sideEffectsVal = self.getSideEffectsMove(move)
+
+        score_baseline = ((me._current_hp/me.max_hp)*100-oppMaxDmg)/100*(move_dmg+sideEffectsVal)*(acc/100)/(oppMaxDmg+1)
+        ##add recovery impact
+        if move_dmg > opp._current_hp:
+            outspeed_benefit = outspeed*MAX_WEIGHT
+        else:
+            outspeed_benefit = 0
+        score = score_baseline+outspeed_benefit
+        return score
 
     def check_if_pokemon_has_effects(self, pokemon:Pokemon):
         if pokemon._status:
@@ -273,14 +263,14 @@ class ReactivePlayer(Player):
                 best_switch = (pokemon_moves[0], pokemon_moves[0].current_hp, max_dmg_move)
         return best_switch
     
-    def print_Weak_Resis_Immun(self, battle, typeMatchup):
-        print("Opponent [" + str(battle.opponent_active_pokemon.species) +"] is: ")
-        print("Weak against -> " + str(typeMatchup[0]))
-        print("Resistant against -> " + str(typeMatchup[1]))
-        print("Immune against -> " + str(typeMatchup[2]) + "\n")
+    def print_Weak_Resis_Immun(self, typeMatchup):
+        print("\nWeaknesses: ", typeMatchup[0])
+        print("Resistances: ", typeMatchup[1])
+        print("Immunities: ", typeMatchup[2])
+        print("***************************************************************************************************")
 
-    def print_team_moves(self, moves):
-        print("Available teammates:")
-        for move in moves[1:]:
-            print(str(move[0]) + "-> " + str([m for m in move[1]]))
-        print("#################################################")
+    def print_team_moves(self, battle):
+        print("\nBenched Pokemon:")
+        for available_switch in battle.available_switches:
+            print(available_switch.species, available_switch._status, str([move for move in available_switch._moves]))
+       
