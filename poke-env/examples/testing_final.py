31e0f796defe0685432569bac38a184c5a3c22e2
Duarte Costa <mestredua@gmail.com>
2023-06-08 00:47:20 +0100

    metrics final

--- poke-env/examples/testing_final.py
+++ poke-env/examples/testing_final.py
@@ -5,15 +5,17 @@ from poke_env.player import RandomPlayer, ReactivePlayer, ProactivePlayer
 from poke_env import LocalhostServerConfiguration, PlayerConfiguration
 from poke_env.teambuilder.gen1ou_team import Team
 
+def print_dict(dict):
+    win_rate = dict["win_rate"]*100
+    print(f"Win Rate: {win_rate:.2f} %")
+    print("Average Number of Turns: " + str(int(dict["battle_duration_avg"])) + " turns")
+    print("Average Pokemon alive: " + str(int(dict["pokemon_alive_avg"])))
 
 async def main_battle(player1, player2, n_battles):
     n = 0
     threshold = time.time()
     battle_duration_total = 0
     pokemon_alive_total = 0
-    damage_dealt_total = 0
-    damage_taken_total = 0
-
     for _ in range(n_battles):
         if n == 5 and (time.time() - threshold) < 181:
             print(time.time() - threshold)
@@ -24,23 +26,19 @@ async def main_battle(player1, player2, n_battles):
             n = 0
             threshold = time.time()
         else:
-            battle_start = time.time()
             await player1.battle_against(player2, 1)
-            battle_duration = time.time() - battle_start
-            battle_duration_total += battle_duration
-
-            #damage_dealt_total += player1._team[0].damage_dealt
-            #damage_taken_total += player1._team[0].damage_taken
-            pokemon_alive = sum(1 for pokemon in player1.player.team if pokemon.current_hp > 0)
-            pokemon_alive_total += pokemon_alive
+            for battle in player1._battles.values():
+                battle_duration_total += battle._turn
+                pokemon_alive = len(battle.available_switches)
+                if "FNT" not in str(battle.active_pokemon._status):
+                    pokemon_alive += 1
+                pokemon_alive_total += pokemon_alive
             n += 1
 
     metrics = {
         "win_rate": player1.n_won_battles / n_battles,
         "battle_duration_avg": battle_duration_total / n_battles,
-        "pokemon_alive_avg": pokemon_alive_total / n_battles,
-        "damage_dealt_total" : damage_dealt_total,
-        "damage_taken_total" : damage_taken_total
+        "pokemon_alive_avg": pokemon_alive_total / n_battles
     }
 
     return metrics
@@ -72,18 +70,24 @@ async def main():
     start = time.time()
     reactive_metrics = await main_battle(reactive_player, random_player, n_battles)
     print(
-        "%s won %d / %d battles against the RandomPlayer [this took %f seconds]"
+        "\n%s won %d / %d battles against the RandomPlayer [this took %f seconds]"
         % ("Reactive Player", reactive_player.n_won_battles, n_battles, time.time() - start)
     )
+
+    print("\nMetrics for Reactive Player against RandomPlayer:")
+    print_dict(reactive_metrics)
 
     ##########################################################################################
 
     start = time.time()
     proactive_metrics = await main_battle(proactive_player, random_player, n_battles)
     print(
-        "%s won %d / %d battles against the RandomPlayer [this took %f seconds]"
+        "\n%s won %d / %d battles against the RandomPlayer [this took %f seconds]"
         % ("Proactive Player", proactive_player.n_won_battles, n_battles, time.time() - start)
     )
+
+    print("\nMetrics for Proactive Player against RandomPlayer:")
+    print_dict(proactive_metrics)
 
     ##########################################################################################
 
@@ -95,7 +99,7 @@ async def main():
     print("##########################################################################################")
 
     print(
-        "%s won %d / %d battles against the Reactive Player [this took %f seconds]"
+        "\n%s won %d / %d battles against the Reactive Player [this took %f seconds]"
         % (
             "Proactive Player",
             proactive_player.n_won_battles,
@@ -112,17 +116,10 @@ async def main():
             n_battles,
             time.time() - start,
         )
-    )
+    )    
 
-    # Print collected metrics
-    print("Metrics for Reactive Player:")
-    print(reactive_metrics)
-
-    print("Metrics for Proactive Player:")
-    print(proactive_metrics)
-
-    print("Metrics for Proactive Player against Reactive Player:")
-    print(proactive_metrics_against_reactive)
+    print("\nMetrics for Proactive Player against Reactive Player:")
+    print_dict(proactive_metrics_against_reactive)
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())    
