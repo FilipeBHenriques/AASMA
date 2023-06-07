d4e7c85838f9b1439ac39d96d1dffd1d642a88d9
Duarte Costa <mestredua@gmail.com>
2023-06-08 00:12:05 +0100

    metrics

--- poke-env/examples/testing_final.py
+++ poke-env/examples/testing_final.py
@@ -2,108 +2,18 @@ import asyncio
 import time
 
 from poke_env.player import RandomPlayer, ReactivePlayer, ProactivePlayer
-from poke_env import LocalhostServerConfiguration, ShowdownServerConfiguration, PlayerConfiguration
+from poke_env import LocalhostServerConfiguration, PlayerConfiguration
+from poke_env.teambuilder.gen1ou_team import Team
 
-
-team_1 = """
-Starmie  
-Ability: none  
-EVs: 252 HP / 252 Def / 252 SpA / 252 SpD / 252 Spe  
-IVs: 2 Atk  
-- Psychic  
-- Blizzard  
-- Thunder Wave  
-- Recover  
-
-Exeggutor  
-Ability: none  
-- Sleep Powder  
-- Psychic  
-- Double-Edge  
-- Explosion  
-
-Alakazam  
-Ability: none  
-EVs: 252 HP / 252 Def / 252 SpA / 252 SpD / 252 Spe  
-IVs: 2 Atk  
-- Psychic  
-- Seismic Toss  
-- Thunder Wave  
-- Recover  
-
-Chansey  
-Ability: none  
-EVs: 252 HP / 252 Def / 252 SpA / 252 SpD / 252 Spe  
-IVs: 2 Atk  
-- Ice Beam  
-- Thunderbolt  
-- Thunder Wave  
-- Soft-Boiled  
-
-Snorlax  
-Ability: none  
-- Body Slam  
-- Reflect  
-- Earthquake  
-- Rest  
-
-Tauros  
-Ability: none  
-- Body Slam  
-- Hyper Beam  
-- Blizzard  
-- Earthquake  
-"""
-
-team_2 = """
-Gengar  
-Ability: none  
-- Hypnosis  
-- Night Shade  
-- Thunderbolt  
-- Explosion  
-
-Zapdos  
-Ability: none  
-- Thunderbolt  
-- Drill Peck  
-- Agility  
-- Thunder Wave  
-
-Articuno  
-Ability: none  
-- Blizzard  
-- Agility  
-- Double-Edge  
-- Hyper Beam  
-
-Chansey  
-Ability: none  
-EVs: 252 HP / 252 Def / 252 SpA / 252 SpD / 252 Spe  
-IVs: 2 Atk  
-- Sing  
-- Ice Beam  
-- Counter  
-- Soft-Boiled  
-
-Snorlax  
-Ability: none  
-- Body Slam  
-- Reflect  
-- Self-Destruct  
-- Rest  
-
-Tauros 
-Ability: none  
-- Body Slam  
-- Hyper Beam  
-- Blizzard  
-- Earthquake  
-"""
 
 async def main_battle(player1, player2, n_battles):
     n = 0
     threshold = time.time()
+    battle_duration_total = 0
+    pokemon_alive_total = 0
+    damage_dealt_total = 0
+    damage_taken_total = 0
+
     for _ in range(n_battles):
         if n == 5 and (time.time() - threshold) < 181:
             print(time.time() - threshold)
@@ -113,9 +23,27 @@ async def main_battle(player1, player2, n_battles):
         elif n == 5:
             n = 0
             threshold = time.time()
-        else:    
+        else:
+            battle_start = time.time()
             await player1.battle_against(player2, 1)
-            n+=1
+            battle_duration = time.time() - battle_start
+            battle_duration_total += battle_duration
+
+            #damage_dealt_total += player1._team[0].damage_dealt
+            #damage_taken_total += player1._team[0].damage_taken
+            pokemon_alive = sum(1 for pokemon in player1.player.team if pokemon.current_hp > 0)
+            pokemon_alive_total += pokemon_alive
+            n += 1
+
+    metrics = {
+        "win_rate": player1.n_won_battles / n_battles,
+        "battle_duration_avg": battle_duration_total / n_battles,
+        "pokemon_alive_avg": pokemon_alive_total / n_battles,
+        "damage_dealt_total" : damage_dealt_total,
+        "damage_taken_total" : damage_taken_total
+    }
+
+    return metrics
 
 async def main():
     n_battles = 1
@@ -123,23 +51,26 @@ async def main():
     random_player = RandomPlayer(
         player_configuration=PlayerConfiguration("random-agnt", "password"),
         server_configuration=LocalhostServerConfiguration,
-        battle_format="gen1ou", 
-        team=team_1)
-    
+        battle_format="gen1ou",
+        team=Team.pick_random_team(),
+    )
+
     reactive_player = ReactivePlayer(
         player_configuration=PlayerConfiguration("reactive-agnt", "password"),
         server_configuration=LocalhostServerConfiguration,
-        battle_format="gen1ou", 
-        team=team_2)
-    
+        battle_format="gen1ou",
+        team=Team.pick_random_team(),
+    )
+
     proactive_player = ProactivePlayer(
         player_configuration=PlayerConfiguration("proactive-agnt", "password"),
         server_configuration=LocalhostServerConfiguration,
-        battle_format="gen1ou", 
-        team=team_2)
-    
+        battle_format="gen1ou",
+        team=Team.pick_random_team(),
+    )
+
     start = time.time()
-    await main_battle(reactive_player, random_player, n_battles)
+    reactive_metrics = await main_battle(reactive_player, random_player, n_battles)
     print(
         "%s won %d / %d battles against the RandomPlayer [this took %f seconds]"
         % ("Reactive Player", reactive_player.n_won_battles, n_battles, time.time() - start)
@@ -148,7 +79,7 @@ async def main():
     ##########################################################################################
 
     start = time.time()
-    await main_battle(proactive_player, random_player, n_battles)
+    proactive_metrics = await main_battle(proactive_player, random_player, n_battles)
     print(
         "%s won %d / %d battles against the RandomPlayer [this took %f seconds]"
         % ("Proactive Player", proactive_player.n_won_battles, n_battles, time.time() - start)
@@ -159,25 +90,39 @@ async def main():
     reactive_player.reset_battles()
     proactive_player.reset_battles()
     start = time.time()
-    await main_battle(proactive_player, reactive_player, n_battles)
-    
+    proactive_metrics_against_reactive = await main_battle(proactive_player, reactive_player, n_battles)
+
     print("##########################################################################################")
 
     print(
         "%s won %d / %d battles against the Reactive Player [this took %f seconds]"
-        % ("Proactive Player", proactive_player.n_won_battles, n_battles, time.time() - start)
+        % (
+            "Proactive Player",
+            proactive_player.n_won_battles,
+            n_battles,
+            time.time() - start,
+        )
     )
 
     print(
         "%s won %d / %d battles against the Proactive Player [this took %f seconds]"
-        % ("Reactive Player", reactive_player.n_won_battles, n_battles, time.time() - start)
+        % (
+            "Reactive Player",
+            reactive_player.n_won_battles,
+            n_battles,
+            time.time() - start,
+        )
     )
 
+    # Print collected metrics
+    print("Metrics for Reactive Player:")
+    print(reactive_metrics)
+
+    print("Metrics for Proactive Player:")
+    print(proactive_metrics)
+
+    print("Metrics for Proactive Player against Reactive Player:")
+    print(proactive_metrics_against_reactive)
+
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(main())
-
-
-
-    
-        
-        
+    asyncio.get_event_loop().run_until_complete(main())    
